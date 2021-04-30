59a0a2b9a0f2fd5fd1cdd4ad5469def94f7ed1c4
Sam <sam.affleck@hotmail.com>
2021-05-01 00:28:01 +0100

    Added argparse and enthalpy plot.

--- LiMnO_MMC_Simulation.py
+++ LiMnO_MMC_Simulation.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import os
 from scipy import integrate
 import time
+import argparse
 
 
 class Atom:
@@ -17,17 +18,15 @@ class Atom:
     Atom class contains the object of a single Li atom
     """
 
-    def __init__(self, x_index, y_index, z_index, grid_length):
+    def __init__(self, x_index, y_index, z_index, grid_length, j1, j2, eps):
 
         self.x_index = x_index
         self.y_index = y_index
         self.z_index = z_index
 
-        self.j1 = 37.5e-3  # Attraction parameter in eV of nearest
-        self.j2 = -4.0e-3  # Attraction parameter in eV of next nearest
-        # self.j1 = 0  # Ideal case
-        # self.j2 = 0  # Ideal case
-        self.eps = 4.12  # In eV
+        self.j1 = j1  # Attraction parameter in eV of nearest
+        self.j2 = j2  # Attraction parameter in eV of next nearest
+        self.eps = eps  # In eV
 
         self.xl = grid_length  # Number of atoms in the x axis
         self.yl = grid_length * 2  # Number of atoms in the y axis
@@ -123,10 +122,6 @@ class Atom:
         self.position = (self.x_index + self.x_offset + self.offset_vector[0], self.y_index * 0.5 +
                          self.offset_vector[1], self.z_index * 0.25 + self.offset_vector[2])
 
-        print("x index: ", self.x_index, "y index: ", self.y_index, "z index: ", self.z_index)
-        print("x pos: ", self.position[0], "y pos: ", self.position[1], "z pos: ", self.position[2])
-        print()
-
     def get_nn(self, grid):
         self.temp_neighbours = []
 
@@ -187,36 +182,7 @@ class Atom:
         return nn_term + nnn_term + potential_term
 
 
-def monte_carlo(grid, grid_length, kb, T, mu, update_occ=False):
-    """
-    This method contains the metropolis monte carlo algorithm and is classed as 1 monte carlo step.
-    """
-    rand_atom = grid[random.randint(0, grid_length - 1)][random.randint(0, (grid_length * 2) - 1)][
-        random.randint(0, (grid_length * 4) - 1)]
-    rand_atom_occ = rand_atom.c
-
-    current_h, random_atom_u = rand_atom.node_hamiltonian(mu)
-    rand_atom.swap_c()
-    swapped = True
-    new_h, new_u = rand_atom.node_hamiltonian(mu)
-    delta_h = current_h - new_h
-
-    if delta_h < 0:
-        # Only keep the swap if a random float between 0 and 1 is <= P
-        rand_p = random.random()  # Generates a random number between 0 and 1
-        p = math.exp(-abs(delta_h) / (kb * T))
-        if rand_p > p:
-            # Don't accept change and reverse swap. So overall no swap is made
-            rand_atom.swap_c()
-            swapped = False
-
-    if update_occ:
-        return grid, rand_atom_occ, swapped, new_u, random_atom_u
-    else:
-        return grid
-
-
-def get_grid(grid_length):
+def get_grid(grid_length, j1, j2, eps):
     """
     Get_grid() returns a 3 dimensional array of 'Atom' objects and is representative of the lattice structure.
     """
@@ -229,7 +195,7 @@ def get_grid(grid_length):
             grid_length * 4):  # Even z layers/indexes are in sub lattice 1 and odd z layers are in sub lattice 2.
         for y in range(grid_length * 2):
             for x in range(grid_length):
-                grid[x][y][z] = Atom(x, y, z, grid_length)
+                grid[x][y][z] = Atom(x, y, z, grid_length, j1, j2, eps)
 
     # Initialise the neighbours for all atoms
     for z in range(grid_length * 4):
@@ -265,9 +231,101 @@ def get_occupancy(grid, gird_length):
     return occupancy
 
 
-def thermal_fluctuations(grid, grid_length, kb, T, mu):
-    sample_frequency = 200
-    total_iterations = 500000
+def get_mole_fraction(grid, grid_length):
+    """
+    Returns the mole fraction of lithium atoms in each sub lattice.
+    """
+    li_count_sl1 = 0
+    li_count_sl2 = 0
+
+    for z in range(grid_length * 4):
+        for y in range(grid_length * 2):
+            for x in range(grid_length):
+                if z % 2 == 0:
+                    li_count_sl1 += grid[x][y][z].c
+                else:
+                    li_count_sl2 += grid[x][y][z].c
+
+    total_num_of_li = li_count_sl1 + li_count_sl2
+    total_num_of_sites = (grid_length ** 3) * 8
+    total_mole_fraction = total_num_of_li / total_num_of_sites
+    print("Li mole fraction in sub lattice 1:", li_count_sl1/(total_num_of_sites/2), " Li mole fraction in sub lattice 2: ",
+          li_count_sl2/(total_num_of_sites/2))
+    print("Total Li atoms:", total_num_of_li, " Total sites:", total_num_of_sites, " Total mole fraction:",
+          total_mole_fraction)
+
+    return total_mole_fraction, li_count_sl1/(total_num_of_sites/2), li_count_sl2/(total_num_of_sites/2)
+
+
+def plot_results(results_array, number_of_mcs):
+
+    results_dataframe = pd.DataFrame(data=results_array, columns=["Delta Entropy", "Mole Fraction of Li",
+                                                                  "Chemical potential", "Mole fraction sub lattice 1",
+                                                                  "Mole fraction sub lattice 2", "dq/de", "Partial molar enthalpy"])
+
+    entropy_list = integrate.cumtrapz(results_array[:, 0], results_array[:, 1], initial=0)  # Contains the entropy values
+    results_dataframe['Entropy'] = entropy_list
+    results_dataframe['Order parameter'] = abs(results_dataframe['Mole fraction sub lattice 1'] - results_dataframe['Mole fraction sub lattice 2'])
+
+    cwd = os.getcwd()
+    path = cwd + "/monte_carlo_results.csv"
+    results_dataframe.to_csv(path, index=False)
+
+    fig, axes = plt.subplots(nrows=2, ncols=2, constrained_layout=True)
+
+    plt.rcParams['font.size'] = 12
+    plt.rcParams['axes.linewidth'] = 2
+
+    fig.suptitle('Number of MCS: %i' % number_of_mcs)
+
+    ax1 = results_dataframe.plot(kind='scatter', ax=axes[0, 0], x='Mole Fraction of Li', y='Chemical potential', color='black', s=4)
+    ax2 = results_dataframe.plot(kind='scatter', ax=axes[0, 1], x='Mole Fraction of Li', y='Delta Entropy', color='black', s=4)
+    ax3 = results_dataframe.plot(kind='scatter', ax=axes[1, 0], x='Mole Fraction of Li',
+                                 y='Mole fraction sub lattice 1', color='red', s=4)
+    results_dataframe.plot(kind='scatter', ax=ax3, x='Mole Fraction of Li', y='Mole fraction sub lattice 2',
+                           color='blue', s=4)
+    ax4 = results_dataframe.plot(kind='scatter', ax=axes[1, 1], x='Mole Fraction of Li', y='Entropy', color='black', s=4)
+
+    ax3.legend(['Sub lattice 1', 'Sub lattice 2'])
+
+    ax1.set_xlim([0, 1])
+    ax2.set_xlim([0, 1])
+    ax3.set_xlim([0, 1])
+    ax4.set_xlim([0, 1])
+
+    ax3.set_xlabel('x')
+    ax4.set_xlabel('x')
+
+    ax1.set_ylabel('E / V vd. Li/Li+')
+    ax2.set_ylabel('dS/dx [kJ/mol K]')
+    ax3.set_ylabel('Sublattice occupancy')
+    ax4.set_ylabel('S [kJ/mol K]')
+
+    fig2, axes2 = plt.subplots(nrows=2, ncols=2, constrained_layout=True)
+    fig2.suptitle('Number of MCS: %i' % number_of_mcs)
+
+    ax5 = results_dataframe.plot(kind='scatter', ax=axes2[0, 0], x='Chemical potential', y='dq/de',
+                                 color='black', s=4)
+    ax6 = results_dataframe.plot(kind='scatter', ax=axes2[0, 1], x='Mole Fraction of Li', y='Order parameter',
+                                 color='black', s=4)
+    ax7 = results_dataframe.plot(kind='scatter', ax=axes2[1, 0], x='Mole Fraction of Li', y='Partial molar enthalpy',
+                                 color='black', s=4)
+
+    ax5.set_xlabel('E ')
+    ax6.set_xlim([0, 1])
+    ax6.set_xlabel('x')
+    ax7.set_xlim([0, 1])
+    ax7.set_xlabel('x')
+
+    ax5.set_ylabel('- dx/dV')
+    ax6.set_ylabel('|n1-n2|')
+    ax6.set_ylabel('Partial molar enthalpy')
+
+    plt.show()
+
+
+def thermal_fluctuations(grid, grid_length, kb, T, mu, mcs, sample_frequency):
+    total_iterations = mcs
     total_rows = total_iterations / sample_frequency
     data_array = np.zeros((int(total_rows), 4))  # Columns for U, N, UN and N^2.
     row_count = 0
@@ -303,50 +361,66 @@ def thermal_fluctuations(grid, grid_length, kb, T, mu):
     return variance, cov_un
 
 
-def get_mole_fraction(grid, grid_length):
+def monte_carlo(grid, grid_length, kb, T, mu, update_occ=False):
     """
-    Returns the mole fraction of lithium atoms in each sub lattice.
+    This method contains the metropolis monte carlo algorithm and is classed as 1 monte carlo step.
     """
-    li_count_sl1 = 0
-    li_count_sl2 = 0
+    rand_atom = grid[random.randint(0, grid_length - 1)][random.randint(0, (grid_length * 2) - 1)][
+        random.randint(0, (grid_length * 4) - 1)]
+    rand_atom_occ = rand_atom.c
 
-    for z in range(grid_length * 4):
-        for y in range(grid_length * 2):
-            for x in range(grid_length):
-                if z % 2 == 0:
-                    li_count_sl1 += grid[x][y][z].c
-                else:
-                    li_count_sl2 += grid[x][y][z].c
+    current_h, random_atom_u = rand_atom.node_hamiltonian(mu)
+    rand_atom.swap_c()
+    swapped = True
+    new_h, new_u = rand_atom.node_hamiltonian(mu)
+    delta_h = current_h - new_h
 
-    total_num_of_li = li_count_sl1 + li_count_sl2
-    total_num_of_sites = (grid_length ** 3) * 8
-    total_mole_fraction = total_num_of_li / total_num_of_sites
-    print("Li mole fraction in sub lattice 1:", li_count_sl1/(total_num_of_sites/2), " Li mole fraction in sub lattice 2: ",
-          li_count_sl2/(total_num_of_sites/2))
-    print("Total Li atoms:", total_num_of_li, " Total sites:", total_num_of_sites, " Total mole fraction:",
-          total_mole_fraction)
+    if delta_h < 0:
+        # Only keep the swap if a random float between 0 and 1 is <= P
+        rand_p = random.random()  # Generates a random number between 0 and 1
+        p = math.exp(-abs(delta_h) / (kb * T))
+        if rand_p > p:
+            # Don't accept change and reverse swap. So overall no swap is made
+            rand_atom.swap_c()
+            swapped = False
 
-    return total_mole_fraction, li_count_sl1/(total_num_of_sites/2), li_count_sl2/(total_num_of_sites/2)
+    if update_occ:
+        return grid, rand_atom_occ, swapped, new_u, random_atom_u
+    else:
+        return grid
 
 
 if __name__ == '__main__':
-    grid_length = 10  # This is the number of unit cells - contains 8 lithium atoms.
-    lattice_points = (grid_length ** 3) * 8
-    T = 298  # Temperature in K
+
+    parser = argparse.ArgumentParser(description='Monte carlo simulation')
+    parser.add_argument('--grid_length', type=int, metavar='', help='Number of unit cells', default=10)
+    parser.add_argument('--temperature', type=int, metavar='', help='Temperature', default=298)
+    parser.add_argument('--eps', type=float, metavar='', help='Epsilon value ', default=4.12)
+    parser.add_argument('--j1', type=float, metavar='', help='J1 interaction parameter for nearest neighbours', default=37.5e-3)
+    parser.add_argument('--j2', type=float, metavar='', help='J2 interaction parameter for next nearest parameters', default=-4.0e-3)
+    parser.add_argument('--mcs', type=int, metavar='', help='Number of monte carlo steps', default=500000)
+    parser.add_argument('--step', type=float, metavar='', help='Chemical potential step size', default=0.01)
+    parser.add_argument('--frequency', type=int, metavar='', help='Number of mcs before a sample is taken', default=200)
+    args = parser.parse_args()
+
     kb = 8.617e-5  # Boltzmann constant in eV/K
-    eps = 4.12  # in eV
-    j1 = 37.5e-3  # Attraction parameter in eV of nearest
-    j2 = -4.0e-3  # Attraction parameter in eV of next nearest
+    grid_length = args.grid_length  # This is the number of unit cells - contains 8 lithium atoms.
+    lattice_points = (grid_length ** 3) * 8
+    T = args.temperature  # Temperature in K
+    eps = args.eps  # in eV
+    j1 = args.j1  # Attraction parameter in eV of nearest
+    j2 = args.j2  # Attraction parameter in eV of next nearest
 
-    grid = get_grid(grid_length)  # Returns a numpy 3d array [x][y][z] of each primitive cell.
+    grid = get_grid(grid_length, j1, j2, eps)  # Returns a numpy 3d array [x][y][z] of each primitive cell.
 
-    number_of_mcs = 500000
+    number_of_mcs = args.mcs
+    sample_frequency = args.frequency
     start_mu = -4.3  # Start chemical potential
     end_mu = - 3.88
-    step_size_mu = 0.005  # Step size for chemical potential
+    step_size_mu = args.step  # Step size for chemical potential
     rows = math.ceil((end_mu - start_mu) / step_size_mu)
     row_count = 0
-    results_array = np.zeros((rows + 1, 6))  # Columns for delta S, x, mu, sl1 mole fraction, sl2 mole fraction
+    results_array = np.zeros((rows + 1, 7))  # Columns for delta S, x, mu, sl1 mole fraction, sl2 mole fraction
 
     for chem in np.arange(start_mu, end_mu + step_size_mu, step_size_mu):
         print("Chemical potential: ", chem)
@@ -358,7 +432,7 @@ if __name__ == '__main__':
         print("Time to finish MC steps: ", finishMC_time - startMC_time)
         total_mole_fraction, sl1_mole_fraction, sl2_mole_fraction = get_mole_fraction(grid, grid_length)  # Prints the final mole fraction
 
-        var, cov = thermal_fluctuations(grid, grid_length, kb, T, chem)
+        var, cov = thermal_fluctuations(grid, grid_length, kb, T, chem, number_of_mcs, sample_frequency)
 
         finishThermo_time = time.time()
         print("Time to finish thermo averaging:", finishThermo_time - finishMC_time)
@@ -370,64 +444,8 @@ if __name__ == '__main__':
         results_array[row_count, 3] = sl1_mole_fraction
         results_array[row_count, 4] = sl2_mole_fraction
         results_array[row_count, 5] = var/(kb * T * lattice_points)
+        results_array[row_count, 6] = cov / var  # Partial molar enthalpy
         row_count += 1
         print("----------")
 
-    results_dataframe = pd.DataFrame(data=results_array, columns=["Delta Entropy", "Mole Fraction of Li",
-                                                                  "Chemical potential", "Mole fraction sub lattice 1",
-                                                                  "Mole fraction sub lattice 2", "dq/de"])
-
-    entropy_list = integrate.cumtrapz(results_array[:, 0], results_array[:, 1], initial=0)  # Contains the entropy values
-    results_dataframe['Entropy'] = entropy_list
-    results_dataframe['Order parameter'] = abs(results_dataframe['Mole fraction sub lattice 1'] - results_dataframe['Mole fraction sub lattice 2'])
-
-    cwd = os.getcwd()
-    path = cwd + "/monte_carlo_results.csv"
-    results_dataframe.to_csv(path, index=False)
-
-    fig, axes = plt.subplots(nrows=2, ncols=2, constrained_layout=True)
-
-    plt.rcParams['font.size'] = 12
-    plt.rcParams['axes.linewidth'] = 2
-
-    fig.suptitle('Number of MCS: %i' %number_of_mcs)
-
-    ax1 = results_dataframe.plot(kind='scatter', ax=axes[0, 0], x='Mole Fraction of Li', y='Chemical potential', color='black', s=4)
-    ax2 = results_dataframe.plot(kind='scatter', ax=axes[0, 1], x='Mole Fraction of Li', y='Delta Entropy', color='black', s=4)
-    ax3 = results_dataframe.plot(kind='scatter', ax=axes[1, 0], x='Mole Fraction of Li',
-                                 y='Mole fraction sub lattice 1', color='red', s=4)
-    results_dataframe.plot(kind='scatter', ax=ax3, x='Mole Fraction of Li', y='Mole fraction sub lattice 2',
-                           color='blue', s=4)
-    ax4 = results_dataframe.plot(kind='scatter', ax=axes[1, 1], x='Mole Fraction of Li', y='Entropy', color='black', s=4)
-
-    ax3.legend(['Sub lattice 1', 'Sub lattice 2'])
-
-    ax1.set_xlim([0, 1])
-    ax2.set_xlim([0, 1])
-    ax3.set_xlim([0, 1])
-    ax4.set_xlim([0, 1])
-
-    ax3.set_xlabel('x')
-    ax4.set_xlabel('x')
-
-    ax1.set_ylabel('E / V vd. Li/Li+')
-    ax2.set_ylabel('dS/dx [kJ/mol K]')
-    ax3.set_ylabel('Sublattice occupancy')
-    ax4.set_ylabel('S [kJ/mol K]')
-
-    fig2, axes2 = plt.subplots(nrows=1, ncols=2, constrained_layout=True)
-    fig2.suptitle('Number of MCS: %i' % number_of_mcs)
-
-    ax5 = results_dataframe.plot(kind='scatter', ax=axes2[0], x='Chemical potential', y='dq/de',
-                                 color='black', s=4)
-    ax6 = results_dataframe.plot(kind='scatter', ax=axes2[1], x='Mole Fraction of Li', y='Order parameter',
-                                 color='black', s=4)
-
-    ax5.set_xlabel('E ')
-    ax6.set_xlim([0, 1])
-    ax6.set_xlabel('x')
-
-    ax5.set_ylabel('- dx/dV')
-    ax6.set_ylabel('|n1-n2|')
-
-    plt.show()
+    plot_results(results_array, number_of_mcs)
