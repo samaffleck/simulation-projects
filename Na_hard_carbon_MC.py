7664ba18231ebdaf99115e36122b5a15d9f32151
Sam <sam.affleck@hotmail.com>
2021-05-13 14:28:29 +0100

    Updated code with 2 energy distributions and all plots updated.

--- Na_hard_carbon_MC.py
+++ Na_hard_carbon_MC.py
@@ -26,10 +26,14 @@ class MonteCarlo:
         self.parser.add_argument('--b', type=float, metavar='', help='b term', default=1.5)
         self.parser.add_argument('--c', type=float, metavar='', help='c term', default=1.0)
         self.parser.add_argument('--sample_frequency', type=int, metavar='', help='Number of mcs before a sample is taken', default=200)
-        self.parser.add_argument('--start_mu', type=float, metavar='', help='starting chemical potential', default=-0.45)
+        self.parser.add_argument('--start_mu', type=float, metavar='', help='starting chemical potential', default=-1.0)
         self.parser.add_argument('--finish_mu', type=float, metavar='', help='starting chemical potential', default=0.0)
-        self.parser.add_argument('--step_chem', type=float, metavar='', help='step size of chemical potential', default=0.01)
+        self.parser.add_argument('--step_chem', type=float, metavar='', help='step size of chemical potential', default=0.04)
         self.parser.add_argument('--T', type=float, metavar='', help='Temperature', default=288)
+        self.parser.add_argument('--eps1_max', type=float, metavar='', help='Maximum point value for interlayers', default=-0.077)
+        self.parser.add_argument('--eps1_min', type=float, metavar='', help='Minimum point value for interlayers', default=-0.677)
+        self.parser.add_argument('--eps1_mean', type=float, metavar='', help='Mean value for interlayers', default=-0.377)
+        self.parser.add_argument('--eps1_sig', type=float, metavar='', help='Standard deviation for the point values for interlayers', default=0.1)
 
         self.args = self.parser.parse_args()
 
@@ -38,9 +42,12 @@ class MonteCarlo:
 
         self.lattice1_sites = int(self.args.sites * self.args.l)  # Number of sites in the interlayers.
         self.lattice2_sites = self.args.sites - self.lattice1_sites  # Number of sites in the nanopores.
-        # self.lattice1 = np.zeros(self.lattice1_sites)  # Interlayers where 0 is an empty site and 1 is a filled Na site.
-        # self.lattice2 = np.zeros(self.lattice2_sites)  # Nanopores.
-        self.lattice = [np.zeros(self.lattice1_sites), np.zeros(self.lattice2_sites)]
+        self.lattice = [np.zeros(self.lattice1_sites), np.zeros(self.lattice2_sites)] # Stores the occupation of all the sites in the lattice. Lattice[0] are the interlayers and lattice[1] are the nanopores.
+        # self.lattice_energy = [np.random.uniform(self.args.eps1_min, self.args.eps1_max, self.lattice1_sites), np.zeros(self.lattice2_sites)]  # Stores the point energy for every site in the lattice.
+        self.lattice_energy = [np.random.normal(self.args.eps1_mean, self.args.eps1_sig, self.lattice1_sites), np.zeros(self.lattice2_sites)]  # Stores the point energy for every site in the lattice.
+
+        for i, site in enumerate(self.lattice_energy[1]):
+            self.lattice_energy[1][i] = self.eps2
 
         self.kb = 8.617e-5  # Boltzmann constant in eV/K
         self.T = self.args.T  # Temperature in K
@@ -55,20 +62,20 @@ class MonteCarlo:
         """
         N1 = np.sum(self.lattice[0])
         N2 = np.sum(self.lattice[1])
-        # n1 = N1/self.lattice1_sites
-        # n2 = N2/self.lattice2_sites
-        # m1 = self.lattice1_sites
-        # m2 = self.lattice2_sites
-        # eps1_prime = self.eps1 + self.args.a * math.exp(-self.args.b * n1 ** self.args.c)
 
-        h = self.eps1 * N1 + self.eps2 * N2 - (N1 + N2) * mu
+        l1_h = np.sum(np.multiply(self.lattice[0], self.lattice_energy[0]))
+        l2_h = np.sum(np.multiply(self.lattice[1], self.lattice_energy[1]))
 
+        h = l1_h + l2_h - (N1 + N2) * mu  # For a random point distribution
+        h2 = self.eps1 * N1 + self.eps2 * N2 - (N1 + N2) * mu  # For a constant point energy
+
+        #print('h:', h, ' h2:', h2)
         return h
 
     def calculate_u(self):
-        N1 = np.sum(self.lattice[0])
-        N2 = np.sum(self.lattice[1])
-        u = self.eps1 * N1 + self.eps2 * N2
+        l1_u = np.sum(np.multiply(self.lattice[0], self.lattice_energy[0]))
+        l2_u = np.sum(np.multiply(self.lattice[1], self.lattice_energy[1]))
+        u = l1_u + l2_u
 
         return u
 
@@ -99,23 +106,28 @@ class MonteCarlo:
         #results_df.plot(linestyle='-', color='green', lw=0.5, marker='o', markeredgecolor='black', markersize=4,
         #                ax=ax3, x='Total mole fraction', y='Nanopore mole fraction')
         ax3 = results_df.plot(linestyle='-', color='blue', lw=0.5, marker='o', markeredgecolor='black',
-                              markersize=4, ax=axes[1, 0], x='Total mole fraction', y='dq/de')
+                              markersize=4, ax=axes[1, 0], x='Chemical potential', y='dq/de')
         ax4 = results_df.plot(linestyle='-', color='black', lw=0.5, marker='o', markeredgecolor='black',
                               markersize=4, ax=axes[1, 1], x='Total mole fraction', y='Partial molar enthalpy')
 
         fig.suptitle('Number of MCS: %i \n Number of lattice points: %i' % (self.args.mcs, self.args.sites))
         ax1.set_xlim([0, 1])
         ax2.set_xlim([0, 1])
-        ax3.set_xlim([0, 1])
         ax4.set_xlim([0, 1])
 
-        ax3.set_xlabel('Na content, x')
+        ax1.set_xlabel('Na content, x')
+        ax2.set_xlabel('Na content, x')
+        ax3.set_xlabel('Voltage V')
         ax4.set_xlabel('Na content, x')
 
         ax1.set_ylabel('Voltage [V]')
         ax2.set_ylabel('dS/dx []')
         ax3.set_ylabel('dq/de')
         ax4.set_ylabel('Partial molar enthalpy []')
+
+        parameter_file = open("Input_arguments.txt", "w")
+        parameter_file.write(str(self.args))
+        parameter_file.close()
 
         fig_path = cwd + "/Na_plot_results.png"
         plt.savefig(fig_path)
