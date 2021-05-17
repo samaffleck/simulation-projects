687a65a72acf773229ec3ae425ace4d9bab01c32
Sam <sam.affleck@hotmail.com>
2021-05-17 17:45:53 +0100

    Small edits to the code.

--- Na_hard_carbon_MC.py
+++ Na_hard_carbon_MC.py
@@ -15,36 +15,66 @@ class MonteCarlo:
 
     def __init__(self):
         self.parser = argparse.ArgumentParser(description="Monte carlo simulation for sodation into hard carbon")
-        self.parser.add_argument('--sites', type=int, metavar='', help='Total number of sites including nanopores and interlayers', default=500)
-        self.parser.add_argument('--l', type=float, metavar='', help='Fraction of total sites in the interlayers', default=0.3)
+        self.parser.add_argument('--sites', type=int, metavar='',
+                                 help='Total number of sites including nanopores and interlayers', default=400)
+        self.parser.add_argument('--l', type=float, metavar='', help='Fraction of total sites in the interlayers',
+                                 default=0.3)
         self.parser.add_argument('--mcs', type=int, metavar='', help='Number of monte carlo steps', default=1000000)
-        self.parser.add_argument('--eps1', type=float, metavar='', help='Point interaction term for interlayers in eV', default=-0.377)
-        self.parser.add_argument('--delE', type=float, metavar='', help='Point term for nanopores - priori heterogeneity in eV', default=-0.25)
+        self.parser.add_argument('--eps1', type=float, metavar='', help='Point interaction term for interlayers in eV',
+                                 default=-0.377)
+        self.parser.add_argument('--delE', type=float, metavar='',
+                                 help='Point term for nanopores - priori heterogeneity in eV', default=-0.25)
         self.parser.add_argument('--g2', type=float, metavar='', help='g2 term', default=0)
         self.parser.add_argument('--g3', type=float, metavar='', help='g3 term', default=0)
         self.parser.add_argument('--a', type=float, metavar='', help='a term', default=0.0)
         self.parser.add_argument('--b', type=float, metavar='', help='b term', default=1.5)
         self.parser.add_argument('--c', type=float, metavar='', help='c term', default=1.0)
-        self.parser.add_argument('--sample_frequency', type=int, metavar='', help='Number of mcs before a sample is taken', default=200)
+        self.parser.add_argument('--sample_frequency', type=int, metavar='',
+                                 help='Number of mcs before a sample is taken', default=200)
         self.parser.add_argument('--start_mu', type=float, metavar='', help='starting chemical potential', default=-1.0)
         self.parser.add_argument('--finish_mu', type=float, metavar='', help='starting chemical potential', default=0.0)
-        self.parser.add_argument('--step_chem', type=float, metavar='', help='step size of chemical potential', default=0.04)
+        self.parser.add_argument('--step_chem', type=float, metavar='', help='step size of chemical potential',
+                                 default=0.04)
         self.parser.add_argument('--T', type=float, metavar='', help='Temperature', default=288)
-        self.parser.add_argument('--eps1_max', type=float, metavar='', help='Maximum point value for interlayers', default=-0.077)
-        self.parser.add_argument('--eps1_min', type=float, metavar='', help='Minimum point value for interlayers', default=-0.677)
-        self.parser.add_argument('--eps1_mean', type=float, metavar='', help='Mean value for interlayers', default=-0.377)
-        self.parser.add_argument('--eps1_sig', type=float, metavar='', help='Standard deviation for the point values for interlayers', default=0.1)
+        self.parser.add_argument('--eps1_max', type=float, metavar='', help='Maximum point value for interlayers',
+                                 default=-0.077)
+        self.parser.add_argument('--eps1_min', type=float, metavar='', help='Minimum point value for interlayers',
+                                 default=-0.677)
+        self.parser.add_argument('--eps1_mean', type=float, metavar='', help='Mean value for interlayers',
+                                 default=-0.377)
+        self.parser.add_argument('--eps1_sig', type=float, metavar='',
+                                 help='Standard deviation for the point values for interlayers', default=0.1)
+        self.parser.add_argument('--eps1_logsig', type=float, metavar='',
+                                 help='Standard deviation for the point values for interlayers in a logarithmic distribution',
+                                 default=1.2)
+        self.parser.add_argument('--mu_list', type=float, nargs='+', metavar='',
+                                 help='Standard deviation for the point values for interlayers in a logarithmic distribution',
+                                 default=[-1.0, -0.99, -0.98, -0.97, -0.96, -0.95, -0.92, -0.88, -0.84, -0.80, -0.76,
+                                          -0.72, -0.68, -0.64, -0.60,
+                                          -0.56, -0.52, -0.5, -0.48, -0.46, -0.44, -0.42, -0.4, -0.38, -0.36, -0.34,
+                                          -0.32, -0.3, -0.28, -0.26, -0.24, -0.22, -0.20, -0.18, -0.16, -0.14, -0.13,
+                                          -0.12, -0.11, -0.10, -0.09, -0.08, -0.07, -0.06, -0.04, -0.02, 0.0])
+        self.parser.add_argument('--normal', type=bool, metavar='', help='True for normal distribution, false for '
+                                                                         'uniform distribution', default=True)
 
         self.args = self.parser.parse_args()
 
         self.eps1 = self.args.eps1
-        self.eps2 = self.args.eps1 - self.args.delE
+        self.eps2 = self.args.eps1_mean - self.args.delE
 
         self.lattice1_sites = int(self.args.sites * self.args.l)  # Number of sites in the interlayers.
         self.lattice2_sites = self.args.sites - self.lattice1_sites  # Number of sites in the nanopores.
-        self.lattice = [np.zeros(self.lattice1_sites), np.zeros(self.lattice2_sites)] # Stores the occupation of all the sites in the lattice. Lattice[0] are the interlayers and lattice[1] are the nanopores.
-        # self.lattice_energy = [np.random.uniform(self.args.eps1_min, self.args.eps1_max, self.lattice1_sites), np.zeros(self.lattice2_sites)]  # Stores the point energy for every site in the lattice.
-        self.lattice_energy = [np.random.normal(self.args.eps1_mean, self.args.eps1_sig, self.lattice1_sites), np.zeros(self.lattice2_sites)]  # Stores the point energy for every site in the lattice.
+        self.lattice = [np.zeros(self.lattice1_sites), np.zeros(
+            self.lattice2_sites)]  # Stores the occupation of all the sites in the lattice. Lattice[0] are the interlayers and lattice[1] are the nanopores.
+
+        if self.args.normal:
+            self.lattice_energy = [np.random.normal(self.args.eps1_mean, self.args.eps1_sig, self.lattice1_sites),
+                                   np.zeros(
+                                       self.lattice2_sites)]  # Stores the point energy for every site in the lattice.
+        else:
+            self.lattice_energy = [np.random.uniform(self.args.eps1_min, self.args.eps1_max, self.lattice1_sites),
+                                   np.zeros(
+                                       self.lattice2_sites)]  # Stores the point energy for every site in the lattice.
 
         for i, site in enumerate(self.lattice_energy[1]):
             self.lattice_energy[1][i] = self.eps2
@@ -66,10 +96,8 @@ class MonteCarlo:
         l1_h = np.sum(np.multiply(self.lattice[0], self.lattice_energy[0]))
         l2_h = np.sum(np.multiply(self.lattice[1], self.lattice_energy[1]))
 
-        h = l1_h + l2_h - (N1 + N2) * mu  # For a random point distribution
-        h2 = self.eps1 * N1 + self.eps2 * N2 - (N1 + N2) * mu  # For a constant point energy
+        h = l1_h + l2_h - (N1 + N2) * mu
 
-        #print('h:', h, ' h2:', h2)
         return h
 
     def calculate_u(self):
@@ -92,8 +120,9 @@ class MonteCarlo:
                                                                "Partial molar enthalpy"])
 
         cwd = os.getcwd()
-        path = cwd + "/Na_monte_carlo_results.csv"
-        results_df.to_csv(path, index=False)
+        # path = cwd + "/Na_monte_carlo_results.csv"
+        results_df.to_csv("C:/Users/samaf/OneDrive/Desktop/Simulation Projects/Na/Na_monte_carlo_results.csv",
+                          index=False)
 
         fig, axes = plt.subplots(nrows=2, ncols=2, constrained_layout=True)
 
@@ -101,10 +130,6 @@ class MonteCarlo:
                               markersize=4, ax=axes[0, 0], x='Total mole fraction', y='Chemical potential')
         ax2 = results_df.plot(linestyle='-', color='black', lw=0.5, marker='o', markeredgecolor='black',
                               markersize=4, ax=axes[0, 1], x='Total mole fraction', y='Partial molar entropy')
-        #ax3 = results_df.plot(linestyle='-', color='blue', lw=0.5, marker='o', markeredgecolor='black',
-        #                      markersize=4, ax=axes[1, 0], x='Total mole fraction', y='Interlayer mole fraction')
-        #results_df.plot(linestyle='-', color='green', lw=0.5, marker='o', markeredgecolor='black', markersize=4,
-        #                ax=ax3, x='Total mole fraction', y='Nanopore mole fraction')
         ax3 = results_df.plot(linestyle='-', color='blue', lw=0.5, marker='o', markeredgecolor='black',
                               markersize=4, ax=axes[1, 0], x='Chemical potential', y='dq/de')
         ax4 = results_df.plot(linestyle='-', color='black', lw=0.5, marker='o', markeredgecolor='black',
@@ -121,16 +146,16 @@ class MonteCarlo:
         ax4.set_xlabel('Na content, x')
 
         ax1.set_ylabel('Voltage [V]')
-        ax2.set_ylabel('dS/dx []')
+        ax2.set_ylabel('dS/dx [eV/Na site]')
         ax3.set_ylabel('dq/de')
-        ax4.set_ylabel('Partial molar enthalpy []')
+        ax4.set_ylabel('Partial molar enthalpy [eV/Na site]')
 
-        parameter_file = open("Input_arguments.txt", "w")
+        parameter_file = open("C:/Users/samaf/OneDrive/Desktop/Simulation Projects/Na/Input_arguments.txt", "w")
         parameter_file.write(str(self.args))
         parameter_file.close()
 
-        fig_path = cwd + "/Na_plot_results.png"
-        plt.savefig(fig_path)
+        # fig_path = cwd + "/Na_plot_results.png"
+        plt.savefig("C:/Users/samaf/OneDrive/Desktop/Simulation Projects/Na/Na_monte_carlo_plot.png")
         plt.show()
 
     def monte_carlo(self, mu):
@@ -163,15 +188,14 @@ class MonteCarlo:
                     self.lattice[lattice_index][random_index] = 0
 
     def run_simulation(self):
-        rows = math.ceil((self.end_mu - self.start_mu) / self.step_size_mu)
+        rows = len(self.args.mu_list) - 1
         row_count = 0
         results_array = np.zeros((rows + 1, 7))  # Columns for delta S, x, mu, sl1 mole fraction, sl2 mole fraction
 
         average_rows = self.args.mcs / self.args.sample_frequency
         data_array = np.zeros((6, int(average_rows)))
 
-        for mu in np.arange(self.start_mu, self.end_mu + self.step_size_mu, self.step_size_mu):
-            mu = round(mu, 3)
+        for mu in self.args.mu_list:
             print("Chemical potential:", mu)
 
             # Runs equilibration steps
@@ -196,16 +220,18 @@ class MonteCarlo:
             mean_n = np.average(data_array[1])
             mean_un = np.average(data_array[2])
             mean_nn = np.average(data_array[3])
-            mean_x1 = np.average(data_array[4])/self.lattice1_sites
-            mean_x2 = np.average(data_array[5])/self.lattice2_sites
-            mean_x = mean_n/self.args.sites
-            variance = mean_nn - mean_n ** 2
+            mean_x1 = np.average(data_array[4]) / self.lattice1_sites
+            mean_x2 = np.average(data_array[5]) / self.lattice2_sites
+            mean_x = mean_n / self.args.sites
+            variance = np.var(data_array[1])
+            # variance = mean_nn - mean_n ** 2
             cov_un = mean_un - (mean_u * mean_n)
 
-            print('Interlayer mole fraction:', mean_x1, 'Nanopore mole fraction:', mean_x2, 'Total mole fraction:', mean_x)
+            print('Interlayer mole fraction:', mean_x1, 'Nanopore mole fraction:', mean_x2, 'Total mole fraction:',
+                  mean_x)
             print("--------")
 
-            delta_entropy = (1 / self.T) * ((cov_un / variance) - mu)
+            delta_entropy = (cov_un / variance) - mu
             results_array[row_count, 0] = mean_x1
             results_array[row_count, 1] = mean_x2
             results_array[row_count, 2] = mean_x
@@ -219,6 +245,5 @@ class MonteCarlo:
 
 
 if __name__ == '__main__':
-
     mc = MonteCarlo()
     mc.run_simulation()
