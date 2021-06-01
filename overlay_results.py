5e9b3e48b4fe1ed2abb36e6c1f8418850bfb0ea8
Sam <sam.affleck@hotmail.com>
2021-06-01 12:55:00 +0100

    Plotting script including ideal case.

--- overlay_results.py
+++ overlay_results.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 
@@ -9,9 +10,21 @@ class DisplayResults:
         pass
 
     def display(self):
-        df1 = pd.read_csv(r"C:\Users\samaf\OneDrive\Desktop\Simulation Projects\Na\Na_monte_carlo_results_uniform_2,2.csv")  # black line
+        df1 = pd.read_csv(r"C:\Users\samaf\OneDrive\Desktop\Simulation Projects\Na\Na_monte_carlo_results_normal_18.csv")  # black line
         df2 = pd.read_csv(r"C:\Users\samaf\OneDrive\Desktop\Simulation Projects\Na\Experimental_data.csv")  # green line
         #df3 = pd.read_csv(r"C:\Users\samaf\OneDrive\Desktop\Simulation Projects\Na\Na_monte_carlo_results_uniform_3,3.csv")  # blue line
+
+        x_pos = np.linspace(0, 1, 50)
+        y_pos = np.linspace(0, 1, 50)
+        l = 0.3
+        for index, x in enumerate(x_pos):
+            if x < l:
+                y_pos[index] = -0.00863 * (np.log(x/l) - np.log((l-x)/l))
+            else:
+                l_prime = (l-x)/l
+                l_2prime = (1-l)/l
+                y_pos[index] = -0.00863 * (np.log(l_2prime) - np.log(-1 * l_prime))
+
 
         fig, axes = plt.subplots(nrows=2, ncols=2, constrained_layout=True)
 
@@ -28,6 +41,7 @@ class DisplayResults:
                  markersize=2, ax=axes[0, 1], x='x', y='Entropy dS/dx')
         #df3.plot(linestyle='-', color='blue', lw=0.3, marker='o', markeredgecolor='blue',
         #         markersize=2, ax=axes[0, 1], x='Total mole fraction', y='Partial molar entropy')
+        ax2.plot(x_pos, y_pos)
 
         ax3 = df1.plot(linestyle='-', color='black', lw=0.3, marker='o', markeredgecolor='black',
                               markersize=2, ax=axes[1, 0], x='Chemical potential', y='dq/de')
@@ -54,11 +68,11 @@ class DisplayResults:
         ax4.set_xlabel('Na content, x')
 
         ax1.set_ylabel('Voltage [V]')
-        ax2.set_ylabel('dS/dx []')
+        ax2.set_ylabel('dS/dx [eV/Na site]')
         ax3.set_ylabel('dq/de')
-        ax4.set_ylabel('Partial molar enthalpy []')
+        ax4.set_ylabel('Partial molar enthalpy [eV/Na site]')
 
-        fig.suptitle('black: eps1=[0.123 to -0.477]\ngreen: eps1=[-0.077 to -0.677]\nblue: eps1=[-0.277 to -0.877]')
+        #fig.suptitle('black: eps1=[0.123 to -0.477]\ngreen: eps1=[-0.077 to -0.677]\nblue: eps1=[-0.277 to -0.877]')
         #ax1.legend(['Uniform distribution [0.077 to 0.677]', 'Normal distrubition, mu=0.377 sig=0.1'])
 
         #plt.savefig(fig_path)
