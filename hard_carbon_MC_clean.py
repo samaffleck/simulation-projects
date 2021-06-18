d63bbbb1302ce5dbb566334356cc08328117e0ab
Sam <sam.affleck@hotmail.com>
2021-06-18 09:56:47 +0100

    Updated default parameters.

--- hard_carbon_MC_clean.py
+++ hard_carbon_MC_clean.py
@@ -1,5 +1,7 @@
 """
 Monte carlo simulation for the insertion of sodium into hard carbon.
+This programme outputs a csv data file that can be run on overlay_results.py.
+This code is suitable for the HEC.
 """
 import uuid
 import numpy as np
@@ -9,28 +11,27 @@ import pandas as pd
 import os
 import argparse
 from pathlib import Path
-from scipy import integrate
-
 
 class MonteCarlo:
 
     def __init__(self):
+        # All arguments are optional and can be passed through the command line.
         self.parser = argparse.ArgumentParser(description="Monte carlo simulation for sodation into hard carbon")
         self.parser.add_argument('--sites', type=int, metavar='',
                                  help='Total number of sites including nanopores and interlayers', default=400)
         self.parser.add_argument('--sps', type=int, metavar='',
                                  help='Number of Monte Carlo steps per site', default=500)
         self.parser.add_argument('--l', type=float, metavar='', help='Fraction of total sites in the interlayers',
-                                 default=0.3)
+                                 default=0.329217689)
         self.parser.add_argument('--eps1', type=float, metavar='', help='Point interaction term for interlayers in eV',
-                                 default=-0.377)
+                                 default=-0.37289072)
         self.parser.add_argument('--eps2', type=float, metavar='',
-                                 help='Point term for nanopores in eV', default=-0.07)
-        self.parser.add_argument('--g2', type=float, metavar='', help='g2 term', default=-0.0421)
-        self.parser.add_argument('--g3', type=float, metavar='', help='g3 term', default=0.0225)
-        self.parser.add_argument('--a', type=float, metavar='', help='a term', default=-0.647)
-        self.parser.add_argument('--b', type=float, metavar='', help='b term', default=2.0)
-        self.parser.add_argument('--c', type=float, metavar='', help='c term', default=1.71)
+                                 help='Point term for nanopores in eV', default=-0.021718645)
+        self.parser.add_argument('--g2', type=float, metavar='', help='g2 term', default=-0.046075307)
+        self.parser.add_argument('--g3', type=float, metavar='', help='g3 term', default=0.032394847)
+        self.parser.add_argument('--a', type=float, metavar='', help='a term', default=-0.684477326)
+        self.parser.add_argument('--b', type=float, metavar='', help='b term', default=1.873973403)
+        self.parser.add_argument('--c', type=float, metavar='', help='c term', default=1.686422347)
         self.parser.add_argument('--sample_frequency', type=int, metavar='',
                                  help='Number of mcs before a sample is taken', default=200)
         self.parser.add_argument('--T', type=float, metavar='', help='Temperature', default=288)
@@ -43,15 +44,17 @@ class MonteCarlo:
         self.parser.add_argument('--eps1_sig', type=float, metavar='',
                                  help='Standard deviation for the point values for interlayers (norm)', default=0.3)
         self.parser.add_argument('--eps1_low', type=float, metavar='',
-                                 help='Most negative interlayer energy (tri)', default=-1.3)
+                                 help='Most negative interlayer energy (tri)', default=-1.65)
         self.parser.add_argument('--eps1_high', type=float, metavar='',
-                                 help='Most positive interlayer energy (tri)', default=-0.1)
+                                 help='Most positive interlayer energy (tri)', default=0.08)
         self.parser.add_argument('--mu_list', type=float, nargs='+', metavar='',
                                  help='List of chemical potentials to loop through',
-                                 default=[-1.2, -1.16, -1.12, -1.08, -1.04, -1.0, -0.96, -0.88, -0.80, -0.72, -0.64,
-                                          -0.56, -0.5, -0.46, -0.42, -0.38, -0.34, -0.3, -0.26, -0.22, -0.20, -0.18,
-                                          -0.16, -0.14, -0.13, -0.12, -0.11, -0.10, -0.09, -0.08, -0.07, -0.06, -0.04,
-                                          -0.02, 0.0])
+                                 default=[-1.6, -1.5, -1.4, -1.35, -1.3, -1.25, -1.2, -1.16, -1.12, -1.08, -1.04, -1.0,
+                                          -0.96, -0.88, -0.80, -0.72, -0.64, -0.56, -0.5, -0.46, -0.42, -0.38, -0.34,
+                                          -0.3, -0.26, -0.22, -0.20, -0.18,-0.16, -0.14, -0.13, -0.125, -0.12, -0.115,
+                                          -0.11, -0.105, -0.10, -0.095, -0.09, -0.085, -0.08, -0.075, -0.07, -0.065,
+                                          -0.06, -0.05, -0.04, -0.03, -0.02, -0.01, 0.0, 0.01, 0.02, 0.03, 0.04, 0.06,
+                                          0.08, 0.1, 0.12])
         self.parser.add_argument('--distribution', type=int, metavar='',
                                  help='Type of distribution: '
                                       '0: Uniform distribution, '
@@ -62,8 +65,8 @@ class MonteCarlo:
 
         self.args = self.parser.parse_args()  # Stores all input parameters
 
-        self.eps1 = self.args.eps1
-        self.eps2 = self.args.eps2
+        self.eps1 = self.args.eps1  # Point term for interlayers
+        self.eps2 = self.args.eps2  # Point term for pores
 
         # Constants for the power distribution.
         self.a = 10
@@ -75,6 +78,7 @@ class MonteCarlo:
 
         # Stores the occupation number of all the sites in the lattice.
         # self.lattice[0]: interlayers and self.lattice[1]: nanopores.
+        # Occupation number of 1 is filled and 0 means vacant.
         self.lattice = [np.zeros(self.lattice1_sites), np.zeros(self.lattice2_sites)]
 
         # Checks what distribution we want to use and initialises randomly the point energies for each lattice.
@@ -121,10 +125,7 @@ class MonteCarlo:
                                                                "dq/de",
                                                                "Partial molar enthalpy"])
 
-        entropy_list = integrate.cumtrapz(results_array[:, 4], results_array[:, 2], initial=0)  # Contains the entropy values
-        results_df['Entropy'] = entropy_list
-
-        uid = str(uuid.uuid1())
+        uid = str(uuid.uuid1())  # Sets a unique id for the naming of files.
         cwd = os.getcwd()
         path = cwd + "/results"
         Path(path).mkdir(parents=True, exist_ok=True)
@@ -158,12 +159,12 @@ class MonteCarlo:
 
     def site_h(self, mu, lattice_index, random_index):
         if self.args.distribution == 4:
-            if lattice_index == 0:  # interlayers
+            if lattice_index == 0:  # Interlayers so pore term will cancel
                 N1 = np.sum(self.lattice[0])  # Number of filled interlayer sites
                 n1 = N1/self.lattice1_sites  # Mole fraction of interlayers
                 eps1 = self.args.eps1 + self.args.a * np.exp(-self.args.b*n1**self.args.c)
                 return eps1 * N1 - self.lattice[lattice_index][random_index] * mu
-            else:
+            else:  # Pores so interlayer term will cancel
                 N2 = np.sum(self.lattice[1])  # Number of filled nanopores
                 n2 = N2/self.lattice2_sites  # Mole fraction of nanopores
                 M2 = self.lattice2_sites
@@ -208,7 +209,7 @@ class MonteCarlo:
                     self.lattice[lattice_index][random_index] = 0
 
     def run_simulation(self):
-        rows = len(self.args.mu_list) - 1
+        rows = len(self.args.mu_list) - 1  # The number of inputs - in this case the number of mu values passed through.
         row_count = 0
         results_array = np.zeros((rows + 1, 7))  # Columns for delta S, x, mu, enthalpy, etc.
 
